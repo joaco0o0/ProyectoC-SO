52e61760e671ad61188e8ac0798d2d6ba00b3d7a
Juanma519 <martindolivo@gmail.com>
2024-05-30 12:47:42 -0300

    Externo, y builtin SIGUE HABIENDO SEGMENTATION FAULT

--- builtin_lookup.c
+++ builtin_lookup.c
@@ -1,17 +1,17 @@
 #include "minish.h"
 
 struct builtin_struct builtin_arr[]= {
-    {"exit", "builtin_exit", "exit [N] - termina el shell, admite un parámetro que es el status de retorno."},
-    {"pid", "builtin_pid", "pid - muestra el process id del shell."},
-    {"uid", "builtin_uid", "uid - muestra el userid como número y también el nombre de usuario."},
-    {"gid", "builtin_gid", "gid - muestra el grupo principal y los grupos secundarios del usuario."},
-    {"getenv", "builtin_getenv", "getenv variable [variable ...] - muestra el valor de dichas variables de ambiente."},
-    {"setenv", "builtin_setenv", "setenv variable valor - define una variable nueva de ambiente o cambia el valor de una variable existente."},
-    {"unsetenv", "builtin_unsetenv", "unsetenv var [var ...] - elimina variables de ambiente."},
-    {"cd", "builtin_cd", "cd [dir] - Cambia el directorio corriente. Opciones:\n cd xxx - Cambia al directorio xxx\n cd     - Cambia al directorio valor de la variable de ambiente HOME\n"},
-    {"status", "builtin_status", "status - muestra el status de retorno del último comando ejecutado."},
-    {"help", "builtin_help", "help [comando] - escribe una ayuda para los comandos."},
-    {"history", "builtin_history", "history [N] - muestra los 10 comandos anteriores."},
+    {"exit", builtin_exit, "exit [N] - termina el shell, admite un parámetro que es el status de retorno."},
+    {"pid", builtin_getpid, "pid - muestra el process id del shell."},
+    {"uid", builtin_getuid, "uid - muestra el userid como número y también el nombre de usuario."},
+    {"gid", builtin_getgid, "gid - muestra el grupo principal y los grupos secundarios del usuario."},
+    {"getenv", builtin_getenv, "getenv variable [variable ...] - muestra el valor de dichas variables de ambiente."},
+    {"setenv", builtin_setenv, "setenv variable valor - define una variable nueva de ambiente o cambia el valor de una variable existente."},
+    {"unsetenv", builtin_unsetenv, "unsetenv var [var ...] - elimina variables de ambiente."},
+    {"cd", builtin_cd, "cd [dir] - Cambia el directorio corriente. Opciones:\n cd xxx - Cambia al directorio xxx\n cd     - Cambia al directorio valor de la variable de ambiente HOME\n"},
+    {"status", builtin_status, "status - muestra el status de retorno del último comando ejecutado."},
+    {"help", builtin_help, "help [comando] - escribe una ayuda para los comandos."},
+    {"history", builtin_history, "history [N] - muestra los 10 comandos anteriores."},
     {NULL, NULL, NULL}, // Marca el final del array
 };
 
